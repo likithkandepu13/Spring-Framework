6e17811fc1a07e062f18bf679eb1af8bfde667c0
Likith Kandepu <146078079+likithkandepu13@users.noreply.github.com>
2024-10-22 17:29:37 +0530

    Delete JFSDS25-Spring-MVC_Hibernate directory

--- JFSDS25-Spring-MVC_Hibernate/src/main/java/com/JFSDS25/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.JFSDS25.dao;
-
-import com.JFSDS25.model.Employee;
-
-public interface EmployeeDao 
-{
-	public void addEmployee(Employee e);
-
-}
--- JFSDS25-Spring-MVC_Hibernate/src/main/java/com/JFSDS25/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.JFSDS25.service;
-
-import com.JFSDS25.model.Employee;
-
-public interface EmployeeService {
-	public void addNewEmployee(Employee e);
-
-}
